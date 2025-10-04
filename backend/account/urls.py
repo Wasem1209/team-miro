7c6e16d284c8ac7efa3bd59f5fc71d0c0b60a6a0
David Gift <giftdavid189@gmail.com>
2025-10-04 19:08:46 +0100

    imlement logout feature

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PaymentMethodViewSet, DriverLicenceViewSet
+from .views import RegisterView, LogoutView, PaymentMethodViewSet, DriverLicenceViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'PaymentMethod', PaymentMethodViewSet)
 router.register(r'DriverLicence', DriverLicenceViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    # registration endpoint
+    path('register/', RegisterView.as_view(), name='register'),
+    # Login endpoint
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Logout endpoint
+    path('logout/', LogoutView.as_view(), name='register'),
+
 ]
