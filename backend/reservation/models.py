dc9a74eced942463386c893191a22d37c443fe10
David Gift <giftdavid189@gmail.com>
2025-10-05 23:42:37 +0100

    implement reservation feature

--- backend/reservation/models.py
+++ backend/reservation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from car.models import Car
+import uuid
 
 # Create your models here.
 
@@ -11,7 +12,7 @@ class Reservation(models.Model):
     )
 
     STATUS_CHOICES = (  
-        ('pennding', 'Pending'),
+        ('pending', 'Pending'),
         ('confirmed', 'Confirmed'),
         ('modified', 'Modified'),
         ('active', 'Active'),
@@ -20,14 +21,15 @@ class Reservation(models.Model):
         ('cancelled', 'Cancelled'),
     )
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     guest_email = models.EmailField(blank=True, null=True)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    reservation_type = models.CharField(choices=RESERVATION_CHOICES)
-    status = models.CharField(choices=STATUS_CHOICES)
+    reservation_type = models.CharField(choices=RESERVATION_CHOICES, max_length=20)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=20, default='pending')
     pickup_location = models.TextField()
     dropoff_location = models.TextField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
