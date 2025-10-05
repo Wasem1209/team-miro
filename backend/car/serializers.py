ca033a60b5740a8072477cf631beff2bb90a6435
David Gift <giftdavid189@gmail.com>
2025-10-05 23:45:14 +0100

    implement car inventory feature

--- backend/car/serializers.py
+++ backend/car/serializers.py
@@ -4,4 +4,4 @@ from .models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = '__all__'
+        fields = ['id', 'name', 'model', 'year', 'colour', 'car_type', 'price_per_day', 'pickup_location', 'status', 'rules', 'seating_capacity', 'luggage_capacity', 'wheel_drive', 'fuel_type', 'transmission', 'photo', 'plate_number', 'created_at', 'updated_at']
