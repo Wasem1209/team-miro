73993ab56d4f642e203d482571f057ae3fe27e6e
David Gift <giftdavid189@gmail.com>
2025-10-11 23:12:25 +0100

    update auth feature

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LogoutView, CustomUserViewSet, PaymentMethodViewSet, DriverLicenceViewSet
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import CustomUserViewSet, PaymentMethodViewSet, DriverLicenceViewSet
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
@@ -13,12 +9,11 @@ router.register(r'DriverLicence', DriverLicenceViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # registration endpoint
-    path('register/', RegisterView.as_view(), name='register'),
-    # Login endpoint
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Logout endpoint
-    path('logout/', LogoutView.as_view(), name='register'),
+
+    # General Djoser endpoint (registration, password reset, etc.)
+    path('auth/', include('djoser.urls')), 
+    
+    # Djoser's JWT-specific endpoints (login, refresh, verify)
+    path('auth/', include('djoser.urls.jwt')),
 
 ]
