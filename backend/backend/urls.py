418ea606cfd577bf5b50a525e837fd86e22e8dec
David Gift <giftdavid189@gmail.com>
2025-10-11 23:13:36 +0100

    update auth feature

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/account/', include('account.urls')),
+    path('api/car/', include('car.urls')),
+    path('api/reservation/', include('reservation.urls')),
 ]
