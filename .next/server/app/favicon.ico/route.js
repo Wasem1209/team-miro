23857494bf348d73fcf681bf5062565c1619bbb4
fabusuyi-deborah <fabusuyideborah03@gmail.com>
2025-10-19 09:04:24 +0100

    remove .next from tracking

--- .next/server/app/favicon.ico/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
-R.c("server/chunks/2617e_next_9dec679d._.js")
-R.c("server/chunks/[root-of-the-server]__4f12bdbe._.js")
-R.m("[project]/Desktop/team-miro/team-miro/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/Desktop/team-miro/team-miro/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/team-miro/team-miro/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/Desktop/team-miro/team-miro/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/team-miro/team-miro/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/static/chunks/Desktop_team-miro_team-miro_src_app_page_tsx_54766228._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Desktop_team-miro_team-miro_src_app_dafd60c6._.js",
-  "static/chunks/2617e_25259d5a._.js"
-],
-    source: "dynamic"
-});
