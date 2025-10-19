26a83a742d987f9236aede152e88699d19aaa997
Ovuchi Ovunda <ovuchiovunda12@gmail.com>
2025-10-19 10:17:48 +0100

    Add footer component

--- .next/static/chunks/Desktop_team-miro_team-miro_src_app_page_tsx_54766228._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/2617e_825224a7._.js",
-  "static/chunks/Desktop_team-miro_team-miro_src_app_dafd60c6._.js"
-],
-    source: "dynamic"
-});
